9dcc3d0c2f7ad1916117fccfb695e1d71342d21a
Nitish7545 <160515639+Nitish7545@users.noreply.github.com>
2024-02-19 21:24:02 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract NetflixAccount {
+    bool public accountOpen;
+
+    event NetflixAccountOpened();
+    event NetflixAccountClosed();
+    event NetflixAccountReverted();
+
+    constructor() {
+        accountOpen = false;
+    }
+
+    function openNetflixAccount() public {
+        require(!accountOpen, "Netflix account is already open");
+        accountOpen = true;
+        emit NetflixAccountOpened();
+    }
+
+    function closeNetflixAccount() public {
+        require(accountOpen, "Netflix account is already closed");
+        accountOpen = false;
+        emit NetflixAccountClosed();
+    }
+
+    function revertNetflixAccount() public {
+        accountOpen = !accountOpen;
+        emit NetflixAccountReverted();
+      
+        if (accountOpen==false) {
+           assert(accountOpen == false); 
+            revert("Netflix account status falsely reverted");
+        }
+    }
+}
